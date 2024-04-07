da5bedb2d018b84922df8be205489a6b6981a9b3
FazeDestiny23 <masterkibo2304@gmail.com>
2024-04-06 21:48:21 -0600

    Comments on Routes.

--- Routes/Routes.swift
+++ Routes/Routes.swift
@@ -7,17 +7,22 @@
 
 import Foundation
 
+// Handles requests to the server routes
 struct Routes {
+    // Instance of UserService
     private let userService = UserService()
     
+    // Handles a generic request to the server
     func handleRequest(_ request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
         URLSession.shared.dataTask(with: request, completionHandler: completion).resume()
     }
     
+    // Gets all users from the server
     func getAllUsers(completion: @escaping ([User]?, Error?) -> Void) {
         let url = URL(string: "http://localhost:3005/users")!
         let request = URLRequest(url: url)
         
+        // Makes a request to fetch all users
         handleRequest(request) { data, _, error in
             if let error = error {
                 completion(nil, error)
@@ -29,6 +34,7 @@ struct Routes {
                 return
             }
             
+            // Parses the response data to get the users
             do {
                 let users = try JSONDecoder().decode([User].self, from: data)
                 completion(users, nil)
@@ -38,10 +44,12 @@ struct Routes {
         }
     }
     
+    // Gets a user by ID from the server
     func getUser(id: Int, completion: @escaping (User?, Error?) -> Void) {
         let url = URL(string: "http://localhost:3005/users/\(id)")!
         let request = URLRequest(url: url)
         
+        // Makes a request to fetch a user by ID
         handleRequest(request) { data, _, error in
             if let error = error {
                 completion(nil, error)
@@ -53,6 +61,7 @@ struct Routes {
                 return
             }
             
+            // Parses the response data to get the user
             do {
                 let user = try JSONDecoder().decode(User.self, from: data)
                 completion(user, nil)
@@ -62,10 +71,12 @@ struct Routes {
         }
     }
     
+    // Adds a new user to the server
     func addUser(user: User, completion: @escaping (Error?) -> Void) {
         var request = URLRequest(url: URL(string: "http://localhost:3005/users/add")!)
         request.httpMethod = "POST"
         
+        // Encodes user data and sets the request body
         do {
             let userData = try JSONEncoder().encode(user)
             request.httpBody = userData
@@ -74,6 +85,7 @@ struct Routes {
             return
         }
         
+        // Makes a request to add a new user
         handleRequest(request) { data, _, error in
             if let error = error {
                 completion(error)
@@ -84,10 +96,12 @@ struct Routes {
         }
     }
     
+    // Updates an existing user on the server
     func updateUser(id: Int, updatedUser: User, completion: @escaping (Error?) -> Void) {
         var request = URLRequest(url: URL(string: "http://localhost:3005/users/update/\(id)")!)
         request.httpMethod = "PUT"
         
+        // Encodes updated user data and sets the request body
         do {
             let updatedUserData = try JSONEncoder().encode(updatedUser)
             request.httpBody = updatedUserData
@@ -96,6 +110,7 @@ struct Routes {
             return
         }
         
+        // Makes a request to update an existing user
         handleRequest(request) { data, _, error in
             if let error = error {
                 completion(error)
@@ -106,10 +121,12 @@ struct Routes {
         }
     }
     
+    // Deletes a user from the server by ID
     func deleteUser(id: Int, completion: @escaping (Error?) -> Void) {
         var request = URLRequest(url: URL(string: "http://localhost:3005/users/delete/\(id)")!)
         request.httpMethod = "DELETE"
         
+        // Makes a request to delete a user by ID
         handleRequest(request) { data, _, error in
             if let error = error {
                 completion(error)
